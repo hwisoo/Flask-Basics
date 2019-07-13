ee6098df2d74464d6d5d50c4eff488ef20b99475
James Cho <hwisoocho@gmail.com>
2019-07-12 21:09:05 -0500

    first index and route

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from Flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
-app.run(debug=True, port=8000)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
