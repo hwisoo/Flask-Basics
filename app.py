1357e501eae8f1b4cc8f49978e59409fa4708509
James Cho <hwisoocho@gmail.com>
2019-07-13 16:55:12 -0500

    flat HTML, render_template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="James"):
-    name = request.args.get('name', name)
-    return "Hello from, {}".format(name)
+    return render_template("index.html", name= name)
 
 
 @app.route('/add/<int:num1>/<int:num2>')
@@ -15,6 +15,7 @@ def index(name="James"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1+num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
