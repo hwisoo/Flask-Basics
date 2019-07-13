3a3579eac3637750ae645e435912c3419f14504b
James Cho <hwisoocho@gmail.com>
2019-07-12 21:28:44 -0500

    request variables

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+def index(name="James"):
+    name = request.args.get('name', name)
+    return "Hello from, {}".format(name)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
